10938cd3f5d08bb9855f3588d8e894aea7ea25f1
Sonja <sonja.ng1@gmail.com>
2022-01-17 16:31:54 -0500

    Setup passport

--- app.js
+++ app.js
@@ -2,19 +2,27 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const db = require ("./config/keys").mongoURI;
-const users = require("./routes/api/users")
+const users = require("./routes/api/users");
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to mongoDB"))
     .catch(err => console.log(err));
 
-app.use("/api/users", users)
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-    debugger;
-    res.send("Hello World!");
-});
+app.use("/api/users", users);
+
+// app.get("/", (req, res) => {
+//     // debugger;
+//     res.send("Hello World");
+// });
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
 
 const port = process.env.PORT || 4000;
 
