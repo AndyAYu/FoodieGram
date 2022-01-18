4d008ca886036e2f3262278838ca1f10bc4f8564
AndyAYu <andyhkgw@gmail.com>
2022-01-18 15:35:41 -0500

    entitie/root finished

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux';
 import session from './session_api_reducer';
 import uiReducer from './ui_reducer';
 import errorsReducer from "./errors_reducer";
+import entities from './entities_reducer'
 
 const RootReducer = combineReducers({
   entities,
