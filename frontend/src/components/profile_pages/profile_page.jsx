a0914a6ad75ad87febd51c8d84c67159000c4869
Jasonchu94 <Jasonchu7272@gmail.com>
2022-01-18 11:02:18 -0500

    add css folders

--- frontend/src/components/profile_pages/profile_page.jsx
+++ frontend/src/components/profile_pages/profile_page.jsx
@@ -5,6 +5,7 @@ import NavBar from '../nav/navbar'
 class ProfilePage extends React.Component{
 
     render(){
+        const {loggedIn} = this.props;
         return(
             <div className='profile-page-container'>
                 <div className='nav-bar-container'>
@@ -17,7 +18,7 @@ class ProfilePage extends React.Component{
                     <div className='profile-posts'>
                         User posts go here
                     </div>
-                    
+                    {loggedIn ? "Friends Bar goes here" : null}
                 </div>
             </div>
         )
