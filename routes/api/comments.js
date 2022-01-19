bcc00de100f991d98adcb286cd863b48b6bbb19b
AndyAYu <andyhkgw@gmail.com>
2022-01-19 11:53:00 -0500

    route api comment done/// changed body:string validation

--- routes/api/comments.js
+++ routes/api/comments.js
@@ -1,55 +1,54 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-const { restart } = require('nodemon');
+// const { restart } = require('nodemon');
 const passport = require('passport');
 
-// const Comment = require('../../models/C');
-// const validatePostInput = require('../../validation/posts');
+const Comment = require('../../models/Comment');
+const validateCommentInput = require('../../validation/comments');
 
-// router.get('/test', (req, res) => {
-//     Post.find()
-//         .sort({ date: -1 })
-//         .then(posts => res.json(posts))
-//         .catch(err => restart.status(404).json({ nopostsfound: 'No posts found' }));
-// });
+router.get('/', (req, res) => {
+    Comment.find()
+        .sort({ date: -1 })
+        .then(comments => res.json(comments))
+        .catch(err => restart.status(404).json({ nocommentsfound: 'No comments found' }));
+});
 
-// router.get('/user/:user_id', (req, res) => {
-//     Post.find({ user: req.params.user_id })
-//         .sort({ date: -1 })
-//         .then(posts => restart.json(posts))
-//         .catch(err =>
-//             res.status(404).json({ nopostsfound: 'No posts found from that user' }
-//             )
-//         );
-// })
+router.get('/:posts/:post_id', (req, res) => {
+    Comment.find({ post: req.params.post_id })
+        .sort({ date: -1 })
+        .then(comments => res.json(comments))
+        .catch(err =>
+            res.status(404).json({ nocommentsfound: 'No comments available' }
+            )
+        );
+})
 
-// router.get('/:id', (req, res) => {
-//     Post.findById(req.params.id)
-//         .then(post => restart.json(post))
-//         .catch(err =>
-//             res.status(404).json({ nopostfound: 'No post found with that ID' })
-//         );
-// });
+router.get('/:id', (req, res) => {
+    Comment.findById(req.params.id)
+        .then(comment => restart.json(comment))
+        .catch(err =>
+            res.status(404).json({ nocommentfound: 'No comment found with that ID' })
+        );
+});
 
-// router.post('/new_post',
-//     passport.authenticate('jwt', { session: false }),
-//     (req, res) => {
-//         const { errors, isValid } = validatePostInput(req.body);
-//         // debugger
-//         if (!isValid) {
-//             return res.status(400).json(errors);
-//         }
+router.post('/new_comment',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        const { errors, isValid } = validateCommentInput(req.body);
+        // debugger
+        if (!isValid) {
+            return res.status(400).json(errors);
+        }
 
-//         const newPost = new Post({
-//             body: req.body.body,
-//             address: req.body.address,
-//             user: req.body.user,
-//             restaurant: req.body.restaurant
-//         });
+        const newComment = new Comment({
+            body: req.body.body,
+            post: req.body.post,
+            user: req.body.user
+        });
 
-//         newPost.save().then(post => res.json(post));
-//     }
-// );
+        newComment.save().then(comment => res.json(comment));
+    }
+);
 
-// module.exports = router;
+module.exports = router;
