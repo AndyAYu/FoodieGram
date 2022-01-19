dde51fdb13d917e4c76922991641c24ae855f96b
AndyAYu <andyhkgw@gmail.com>
2022-01-18 22:33:50 -0500

    post router

--- models/Post.js
+++ models/Post.js
@@ -18,6 +18,10 @@ const PostSchema = new Schema ({
         type: String,
         required: true
     },
+    date: {
+        type: Date,
+        default: Date.now
+    },
     user: { type: Schema.Types.ObjectId, 
         ref: 'User' 
     },
