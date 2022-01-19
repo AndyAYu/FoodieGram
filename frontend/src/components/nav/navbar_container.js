d15a0affdd02cbde6fc846386ebcf56220b1bafa
Jasonchu94 <Jasonchu7272@gmail.com>
2022-01-19 12:12:16 -0500

    merge friends

--- frontend/src/components/nav/navbar_container.js
+++ frontend/src/components/nav/navbar_container.js
@@ -7,7 +7,7 @@ import NavBar from './navbar';
 const mapStateToProps = state => {
     let currentUserId
     if(state.session.user){
-        currentUserId = state.session.user.id
+        currentUserId = state.session.user.id 
     }
     return(
         {
