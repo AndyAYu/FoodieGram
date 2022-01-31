17c5f7b139576744804f93fff383e84b88012218
Jasonchu94 <Jasonchu7272@gmail.com>
2022-01-31 14:02:17 -0800

    complete search bar functionality

--- frontend/src/components/search_result/search_result.jsx
+++ frontend/src/components/search_result/search_result.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 class SearchResult extends React.Component{
 
     
@@ -9,10 +9,29 @@ class SearchResult extends React.Component{
     render(){
         if(this.props.users.length === 0) return null;
         const {users} = this.props
-        debugger
+        let allHandles = users.map(user=>(
+            user.handle
+        ))
+        let searchTerms = this.props.location.search.split('=')[1]
+        let searchUsers = users.filter(user=>{
+            if (user.handle === searchTerms) return user
+        })
+        // debugger
         return(
-            <div>
-
+            <div className='search-result-container'>
+                {searchUsers.map(user=>{
+                    // debugger
+                    return(
+                        <div className='search-result-list-items'>
+                            {user.handle}
+                            <Link to={`/pages/${users.indexOf(user)}`}>{user.handle}</Link>
+                            HELLO
+                        </div>
+                    )
+                   
+                }
+                    
+                )}
             </div>
         )
     }
