4c712bbc85f2845e62c9f6313a9505528c7a4585
AndyAYu <andyhkgw@gmail.com>
2022-02-01 17:54:30 -0500

    pictureuplodaer

--- frontend/src/components/profile_pages/profile_page.jsx
+++ frontend/src/components/profile_pages/profile_page.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PostIndexContainer from '../post/post_index_container';
-
+import PictureUploaderContainer from '../pictureuploader/pictureuploader';
 import FriendsBarContainer from '../friends/friends_bar_container';
 import Newsfeed from '../news_feed/news_feed';
 // import FriendsBar from '../friends/friends_bar';
@@ -55,8 +55,6 @@ class ProfilePage extends React.Component{
                 <div className='profile-info-container'>
                     <div className='profile-picture'>
                         <img src='https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/637057ab-e96d-4cef-8e18-fe67f4bfb343/de7am4n-8729f211-870d-4475-b8b7-fe525854e8f8.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzYzNzA1N2FiLWU5NmQtNGNlZi04ZTE4LWZlNjdmNGJmYjM0M1wvZGU3YW00bi04NzI5ZjIxMS04NzBkLTQ0NzUtYjhiNy1mZTUyNTg1NGU4ZjgucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.ukD5SFWZwoj_ZoPDuieCTdFaiN0vOehbQ9-mq3z-ycQ'></img>
-                     
-                     
                         {/* <div>Shortcuts</div> */}
                         <div className='profile-name'> {upperCaseName}</div>
                         <div className="about-me">Hi my name is {upperCaseName} and I am a foodie from New York!</div>
